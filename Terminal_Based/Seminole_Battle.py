e417829994503fdb2002a896fade92f2dc971848
Diego <diegopenuelagonzalez@gmail.com>
2025-04-28 21:32:04 -0400

    Fixed seminole_battle

--- Terminal_Based/Seminole_Battle.py
+++ Terminal_Based/Seminole_Battle.py
@@ -13,17 +13,17 @@ while True:
     """
     difficulty = input("1. Easy\n2. Medium\n3. Hard\n\nChoose a Difficulty: ")
 
-    if difficulty == "1" or "Easy" or "easy":
+    if (difficulty == "1") or (difficulty == "Easy") or (difficulty == "easy"):
         player = Character(60)
         seminole = Enemy(50, 13)
         break
 
-    elif difficulty == "2" or "Medium" or "medium":
+    elif (difficulty == "2") or (difficulty == "Medium") or (difficulty == "medium"):
         player = Character(50)
         seminole = Enemy(55, 15)
         break
 
-    elif difficulty == "3" or "Hard" or "hard":
+    elif (difficulty == "3") or (difficulty == "Hard") or (difficulty == "hard"):
         player = Character(40)
         seminole = Enemy(60, 20)
         break
@@ -45,15 +45,15 @@ while True:
     print()
     weapon_choice = input("1. crossbow\n2. sword\n3. shield\n\nChoose a Weapon for Your Battle: ")
 
-    if weapon_choice == "1" or "crossbow":
+    if (weapon_choice == "1") or (weapon_choice == "crossbow"):
         weapon = crossbow
         break
 
-    elif weapon_choice == "2" or "sword":
+    elif (weapon_choice == "2") or (weapon_choice == "sword"):
         weapon = sword
         break
 
-    elif weapon_choice == "3" or "shield":
+    elif (weapon_choice == "3") or (weapon_choice == "shield"):
         weapon = shield
         break
 
@@ -72,7 +72,7 @@ while (seminole.health > 0) and (player.health > 0):
     attack = input("Do you wish to attack?(Y/N) ")
     print()
 
-    if attack == "y" or "Y":
+    if (attack == "y") or (attack == "Y"):
         damage_to_player = random.choice(range(seminole.attack)) + (seminole.attack - weapon.defense)
         damage_to_enemy = random.choice(range(weapon.attack)) + weapon.attack
 
@@ -88,7 +88,7 @@ while (seminole.health > 0) and (player.health > 0):
         print(f"THE Seminole Indian dealt {damage_to_player} damage, so you now have {player.health} health.")
         print(f"You dealt {damage_to_enemy} damage with your {weapon}, so THE Seminole Indian now has {seminole.health} health.")
 
-    elif attack == "n" or "N":
+    elif (attack == "n") or (attack == "N"):
         print("You do nothing. But why? Why would you do that?")
         damage_to_player = random.choice(range(seminole.attack)) + (seminole.attack - weapon.defense)
         player.take_damage(damage_to_player)
@@ -122,11 +122,11 @@ But QUICK, you guys have been found out. Now find you're way out of the hideout.
             run = input("Your health is getting low. Do you wish to run?(Y/N) ")
             print()
 
-            if run == "y" or "Y":
+            if (run == "y") or (run == "Y"):
                 print("Wow. Pretty messed up. I know you only had " + str(player.health), "health, but still.")
                 print("Well, you run away and never see Albert and Alberta again. UF has deemed you a traitor.")
                 print("\nThe Unsatisfying Ending")
                 break
 
-            elif run == "n" or "N":
+            elif (run == "n") or (run == "N"):
                 print("Very brave. Very admirable. You keep on fighting.")
